17bf010c3225e7c7fd23388e2a0fd6be73d54671
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-15 00:53:19 +0200

    done clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 class Search extends Component {
   state = {
     text: "",
@@ -7,8 +7,9 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired
-  }
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
 
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
@@ -19,10 +20,11 @@ class Search extends Component {
     this.props.searchUsers(this.state.text);
     this.setState({ text: "" });
   };
-  //here we are passing props up to the App.js component. 
+  //here we are passing props up to the App.js component.
   // we will have ghe searchUsers function there
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -39,7 +41,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
-        <button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
