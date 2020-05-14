81fea17dce3abc6f13e9b0271ae35fb158ab8c97
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-05-14 19:39:59 +0200

    display somee users. hardcode data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends Component {
   render() {
@@ -9,7 +9,9 @@ class App extends Component {
     return ( 
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" /> 
-        <UserItem />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     )
   }
